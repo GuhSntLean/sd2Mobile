b575a41149a7c2dd5170e090c0c26278171116a9
GuhSntLean <guleandro.66@gmail.com>
2021-01-13 15:20:06 -0300

    Add homepage e header

--- App.tsx
+++ App.tsx
@@ -1,12 +1,26 @@
+import { useFonts, OpenSans_700Bold, OpenSans_400Regular } from '@expo-google-fonts/open-sans';
+import AppLoading from 'expo-app-loading';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Header from './src/component/Header';
+import Home from './src/pages/Home';
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    OpenSans_700Bold,
+    OpenSans_400Regular
+  });
+
+  if(!fontsLoaded){
+    return <AppLoading />;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Header/>
+      <Home/>
     </View>
   );
 }
@@ -14,8 +28,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
